faef00eb8bf9a551d98e428f365035968ed68573
Nandha Kumar <76443408+nandha-batzzy@users.noreply.github.com>
2020-12-24 20:32:29 +0530

    Add files via upload

---
+++ Making Triangles.py
@@ -0,0 +1,48 @@
+"""k = 10
+for i in range(0, k):
+        n = k - i - 1
+        for j in range(0, n):
+                print(' ', end="")
+        for e in range(k-n):
+                print('*',end="")
+        print('\r')"""
+
+"""n =10
+for i in range(0, n):
+      k = 1
+      for j in range(0, i+1):
+              print(k,end="")
+              k = k+1
+      print('\r')"""
+
+"""t = 11
+n = int(t/2)
+for i in range(0,n):
+        k = n-i-1
+        for j in range(0,k):
+                print(" ", end="")
+
+        for l in range(0,(n-k)):
+                print('*', end="")
+        for x in range(n,(n+i)):
+                print("*",end="")
+        print('\r')"""
+
+
+"""n =10
+for i in range(0, n):
+      for j in range(0, i+1):
+              print("*",end="")
+              
+      print('\r')"""
+
+
+
+
+
+
+
+
+
+
+
