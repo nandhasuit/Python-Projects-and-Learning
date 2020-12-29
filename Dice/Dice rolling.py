8d08145f216e186e3041c078623e44c7e58e4fbc
Nandha Kumar <76443408+nandha-batzzy@users.noreply.github.com>
2020-12-29 23:38:32 +0530

    Add files via upload

---
+++ Dice/Dice rolling.py
@@ -0,0 +1,30 @@
+from tkinter import *
+import  random
+from PIL import Image, ImageTk
+
+root = Tk()
+root.geometry('400*400')
+root.title("Dice Game")
+Label(root, text = '').pack()
+Label(root, text= 'Hi from nandha',font = ('Helvetica', 12), fg= 'black', bg = 'light green' ).pack()
+
+dice = ['A1.png', 'A2.png', 'A3.png', 'A4..png', 'A5.png', 'A6.png']
+diceimg = ImageTk.PhotoImage(Image.open(random.choice(dice)))
+imgla = Lable(root, image = diceimg)
+imgla.image = diceimg
+
+Imgla.pack(expand = True)
+def rolling_dice():
+    diceimg = ImageTk.PhotoImage(Image.open(random.choice(dice)))
+
+    imgla.configure(image=DiceImage)
+    # keep a reference
+    imgla.image = DiceImage
+# adding button, and command will use rolling_dice function
+button = tkinter.Button(root, text='Roll the Dice', fg='blue', command=rolling_dice)
+
+# pack a widget in the parent widget
+button.pack( expand=True)
+
+root.mainloop()
+
