0e2dba4ff85b11809edc12c2fefe93853778b2d6
Nandha Kumar <76443408+nandha-batzzy@users.noreply.github.com>
2020-12-31 12:44:59 +0530

    Update Caesar Cipher.py

--- Caesar Cipher.py
+++ Caesar Cipher.py
@@ -1,12 +1,16 @@
-str1 ='xyz'
+str1 ='abc'
 n = 4
 a= 'abcdefghijklmnopqrstuvwxyz'
 c=""
+m = 0
 for i in range(len(str1)):
     for j in range(len(a)):
         if str1[i] == a[j]:
-            m = j+n
-            c = c+a[m]
+            if (j+n) <= 26:
+                m = j+n
+            elif (j+n) >= 26:
+                m= j+n-26
+            c = c+a [m]
 print(c)
 
 
