b9a3eab160ddf435ba12d5150f7f854e22335d80
Nandha Kumar <76443408+nandha-batzzy@users.noreply.github.com>
2021-01-12 23:11:43 +0530

    Add files via upload

---
+++ Code forces Simple probs/ep1_Water mellon (yes or no).py
@@ -0,0 +1,10 @@
+'''One hot summer day Pete and his friend Billy decided to buy a watermelon. They chose the biggest and the ripest one, in their opinion. After that the watermelon was weighed, and the scales showed w kilos. They rushed home, dying of thirst, and decided to divide the berry, however they faced a hard problem.
+
+Pete and Billy are great fans of even numbers, that's why they want to divide the watermelon in such a way that each of the two parts weighs even number of kilos, at the same time it is not obligatory that the parts are equal. The boys are extremely tired and want to start their meal as soon as possible, that's why you should help them and find out, if they can divide the watermelon in the way they want. For sure, each of them should get a part of positive weight.'''
+
+weight = int(input('Enter the weight of the water mwlon: '))
+
+if weight%2 == 0:
+    print('Yes, you can divide the water melon evenly ')
+else:
+    print('No, you cannot divide the water melon evenly ')
