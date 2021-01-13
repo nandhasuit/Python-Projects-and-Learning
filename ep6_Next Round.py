d7e272ceaf78f0ee642d5cbc21ec7999ee44c86d
Nandha Kumar <76443408+nandha-batzzy@users.noreply.github.com>
2021-01-13 22:55:45 +0530

    Add files via upload

---
+++ ep6_Next Round.py
@@ -0,0 +1,22 @@
+'''"Contestant who earns a score equal to or greater than the k-th place finisher's score will advance to the next round, as long as the contestant earns a positive score..." — an excerpt from contest rules.
+
+A total of n participants took part in the contest (n ≥ k), and you already know their scores. Calculate how many participants will advance to the next round.
+
+Input
+The first line of the input contains two integers n and k (1 ≤ k ≤ n ≤ 50) separated by a single space.
+
+The second line contains n space-separated integers a1, a2, ..., an (0 ≤ ai ≤ 100), where ai is the score earned by the participant who got the i-th place. The given sequence is non-increasing (that is, for all i from 1 to n - 1 the following condition is fulfilled: ai ≥ ai + 1).
+
+Output
+Output the number of participants who advance to the next round.'''
+
+cont = int(input("Enter the Number of contestant: "))
+k_score = int(input("Enter the kth player score"))
+
+cont_scores = [input("Enter the contestant scores") for i in range(cont)]
+c = 0
+for i in range(cont):
+    if int(cont_scores[i]) > k_score:
+        c = c+1
+
+print(c)
