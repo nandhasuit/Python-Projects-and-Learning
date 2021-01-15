13b331a154b92bd38d0d81dfe9273f243291e83e
Nandha Kumar <76443408+nandha-batzzy@users.noreply.github.com>
2021-01-15 21:39:57 +0530

    Add files via upload

---
+++ Code forces Simple probs/ep9_Petya and Strings.py
@@ -0,0 +1,12 @@
+strn1 = input("Enter the first string: ").lower()
+strn2 = input("Enter the second string: ").lower()
+
+
+if strn1 > strn2:
+    print(1)
+elif strn1 < strn2:
+    print(-1)
+elif strn1 == strn2:
+    print(0)
+
+
